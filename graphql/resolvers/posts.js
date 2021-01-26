ee47426b5db876c1cc1618f9c78372d5448209ce
Kamil <tomala.kamil@hotmail.com>
2021-01-26 18:22:08 +0100

    Authentication - part1

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -9,6 +9,23 @@ module.exports = {
             } catch(error) {
                 throw new Error(error);
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if(post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }
+            } catch(err) {
+                throw new Error(err);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            
         }
     }
-}
+};
