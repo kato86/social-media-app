9324da2f2415f25e1df82cc74be5b4a6a48227d7
Kamil <tomala.kamil@hotmail.com>
2021-02-01 22:22:55 +0100

    Created/Deleted posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,13 @@
+const { AuthenticationError } = require('apollo-server');
+
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createAt: -1 });
                 return posts;
             } catch(error) {
                 throw new Error(error);
@@ -25,7 +28,33 @@ module.exports = {
     },
     Mutation: {
         async createPost(_, { body }, context) {
-            
+            const user = checkAuth(context);
+            console.log(user);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+            try {
+                const post = await Post.findById(postId);
+                if(user.username === post.username) {
+                    await post.delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+            } catch(err) {
+                throw new Error(err);
+            }
         }
     }
 };
